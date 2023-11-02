82d166f54adb36eb417740a43147d291e98b0a59
xmartina <martinsprince126@gmail.com>
2023-11-02 15:04:41 +0100

    fixed all files

--- resources/views/admin/layouts/common/header_script.blade.php
+++ resources/views/admin/layouts/common/header_script.blade.php
@@ -7,7 +7,7 @@
         @if(isset($setting))
         <!-- App Title -->
         <title>@yield('title') | {{ $setting->meta_title ?? '' }}</title>
-        
+
         <meta name="description" content="{!! str_limit(strip_tags($setting->meta_description), 160, ' ...') !!}">
         <meta name="keywords" content="{!! strip_tags($setting->meta_keywords) !!}">
 
@@ -35,7 +35,77 @@
         <!-- toastr css -->
         <link rel="stylesheet" href="{{ asset('dashboard/plugins/toastr/css/toastr.min.css') }}">
 
-        
+{{--        Included Template Files--}}
+
+        <link rel="stylesheet" href="{{ asset('dashboard/plugins/toastr/css/toastr.min.css') }}">
+
+        <link href="{{ asset('dashboard/tpl/css/jquery-ui.min.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('dashboard/tpl/css/animate.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('dashboard/tpl/css/css-plugin-collections.css')}}" rel="stylesheet"/>
+        <!-- CSS | menuzord megamenu skins -->
+        <link id="menuzord-menu-skins" href="{{ asset('dashboard/tpl/css/menuzord-skins/menuzord-rounded-boxed.css')}}" rel="stylesheet"/>
+        <!-- CSS | Main style file -->
+        <link href="{{ asset('dashboard/tpl/css/style-main.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('dashboard/tpl/css_extend/main.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('dashboard/tpl/css_extend/normalize.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('dashboard/tpl/css_extend/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+
+        <link href="{{ asset('dashboard/tpl/css_extend/slick-slider.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('dashboard/tpl/css_extend/responsive.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('dashboard/tpl/css_extend/color.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('dashboard/tpl/css_extend/color-two.css')}}" rel="stylesheet">
+        <link href="{{ asset('dashboard/tpl/css_extend/color-three.css')}}" rel="stylesheet">
+        <link href="{{ asset('dashboard/tpl/css_extend/color-four.css')}}" rel="stylesheet">
+        <link href="{{ asset('dashboard/tpl/css_extend/bootstrap.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('dashboard/tpl/css_extend/mater.html')}}" rel="stylesheet" type="text/css">
+        <!-- CSS | Custom Margin Padding Collection -->
+        <link href="{{ asset('dashboard/tpl/css/custom-bootstrap-margin-padding.css')}}" rel="stylesheet" type="text/css">
+        <!-- CSS | Responsive media queries -->
+        <link href="{{ asset('dashboard/tpl/css/responsive.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('dashboard/tpl/css_extend/responsive.css')}}" rel="stylesheet" type="text/css">
+        <!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
+        <!-- <link href="css/style.css" rel="stylesheet" type="text/css"> -->
+
+        <!-- Revolution Slider 5.x CSS settings -->
+        <link  href="{{ asset('dashboard/tpl/js/revolution-slider/css/settings.css')}}" rel="stylesheet" type="text/css"/>
+        <link  href="{{ asset('dashboard/tpl/js/revolution-slider/css/layers.css')}}" rel="stylesheet" type="text/css"/>
+        <link  href="{{ asset('dashboard/tpl/js/revolution-slider/css/navigation.css')}}" rel="stylesheet" type="text/css"/>
+
+        <!-- CSS | Theme Color -->
+        <link href="{{ asset('dashboard/tpl/css/colors/theme-skin-color-set-1.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('dashboard/tpl/css_extend/lecture.css')}}" rel="stylesheet" type="text/css">
+
+
+        <link href="{{ asset('dashboard/tpl/css_extend/styles.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('dashboard/tpl/css_extend/style.css')}}" rel="stylesheet" type="text/css">
+
+
+        <!-- external javascripts -->
+        <script src="{{ asset('dashboard/tpl/js/jquery-2.2.4.min.js')}}"></script>
+        <script src="{{ asset('dashboard/tpl/js/jquery-ui.min.js')}}"></script>
+        <script src="{{ asset('dashboard/tpl/js/bootstrap.min.js')}}"></script>
+        <!-- JS | jquery plugin collection for this theme -->
+        <script src="{{ asset('dashboard/tpl/js/jquery-plugin-collection.js')}}"></script>
+
+        <!-- Revolution Slider 5.x SCRIPTS -->
+        <script src="{{ asset('dashboard/tpl/js/revolution-slider/js/jquery.themepunch.tools.min.js')}}"></script>
+        <script src="{{ asset('dashboard/tpl/js/revolution-slider/js/jquery.themepunch.revolution.min.js')}}"></script>
+
+
+
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        <link href="{{ asset('dashboard/tpl/css/prince.css')}}" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="{{ asset('dashboard/tpl/css/prince-2.css')}}">
+
+{{--        End Included Template Files--}}
+
+
         <!-- page css -->
         @yield('page_css')
 
@@ -43,8 +113,8 @@
         <!-- vendor css -->
         <link rel="stylesheet" href="{{ asset('dashboard/css/style.css') }}" type="text/css" media="screen, print">
 
-        @php 
-        $version = App\Models\Language::version(); 
+        @php
+        $version = App\Models\Language::version();
         @endphp
         @if($version->direction == 1)
         <!-- RTL css -->
--- resources/views/web/layouts/master.blade.php
+++ resources/views/web/layouts/master.blade.php
@@ -27,7 +27,7 @@
 
     <!-- Social Meta Tags -->
     <link rel="canonical" href="{{ route('home') }}">
-    
+
     @yield('social_meta_tags')
 
 
@@ -44,8 +44,101 @@
     <link rel="stylesheet" href="{{ asset('web/css/responsive.css') }}">
 
 
-    @php 
-    $version = App\Models\Language::version(); 
+
+     {{--        Included Template Files--}}
+
+     <link rel="stylesheet" href="{{ asset('dashboard/plugins/toastr/css/toastr.min.css') }}">
+
+     <link href="{{ asset('dashboard/tpl/css/jquery-ui.min.css')}}" rel="stylesheet" type="text/css">
+     <link href="{{ asset('dashboard/tpl/css/animate.css')}}" rel="stylesheet" type="text/css">
+     <link href="{{ asset('dashboard/tpl/css/css-plugin-collections.css')}}" rel="stylesheet"/>
+     <!-- CSS | menuzord megamenu skins -->
+     <link id="menuzord-menu-skins" href="{{ asset('dashboard/tpl/css/menuzord-skins/menuzord-rounded-boxed.css')}}" rel="stylesheet"/>
+     <!-- CSS | Main style file -->
+     <link href="{{ asset('dashboard/tpl/css/style-main.css')}}" rel="stylesheet" type="text/css">
+     <link href="{{ asset('dashboard/tpl/css_extend/main.css')}}" rel="stylesheet" type="text/css">
+     <link href="{{ asset('dashboard/tpl/css_extend/normalize.css')}}" rel="stylesheet" type="text/css">
+     <link href="{{ asset('dashboard/tpl/css_extend/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+
+     <link href="{{ asset('dashboard/tpl/css_extend/slick-slider.css')}}" rel="stylesheet" type="text/css">
+     <link href="{{ asset('dashboard/tpl/css_extend/responsive.css')}}" rel="stylesheet" type="text/css">
+     <link href="{{ asset('dashboard/tpl/css_extend/color.css')}}" rel="stylesheet" type="text/css">
+     <link href="{{ asset('dashboard/tpl/css_extend/color-two.css')}}" rel="stylesheet">
+     <link href="{{ asset('dashboard/tpl/css_extend/color-three.css')}}" rel="stylesheet">
+     <link href="{{ asset('dashboard/tpl/css_extend/color-four.css')}}" rel="stylesheet">
+     <link href="{{ asset('dashboard/tpl/css_extend/bootstrap.css')}}" rel="stylesheet" type="text/css">
+     <link href="{{ asset('dashboard/tpl/css_extend/mater.html')}}" rel="stylesheet" type="text/css">
+     <!-- CSS | Custom Margin Padding Collection -->
+     <link href="{{ asset('dashboard/tpl/css/custom-bootstrap-margin-padding.css')}}" rel="stylesheet" type="text/css">
+     <!-- CSS | Responsive media queries -->
+     <link href="{{ asset('dashboard/tpl/css/responsive.css')}}" rel="stylesheet" type="text/css">
+     <link href="{{ asset('dashboard/tpl/css_extend/responsive.css')}}" rel="stylesheet" type="text/css">
+     <!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
+     <!-- <link href="css/style.css" rel="stylesheet" type="text/css"> -->
+
+     <!-- Revolution Slider 5.x CSS settings -->
+     <link  href="{{ asset('dashboard/tpl/js/revolution-slider/css/settings.css')}}" rel="stylesheet" type="text/css"/>
+     <link  href="{{ asset('dashboard/tpl/js/revolution-slider/css/layers.css')}}" rel="stylesheet" type="text/css"/>
+     <link  href="{{ asset('dashboard/tpl/js/revolution-slider/css/navigation.css')}}" rel="stylesheet" type="text/css"/>
+
+     <!-- CSS | Theme Color -->
+     <link href="{{ asset('dashboard/tpl/css/colors/theme-skin-color-set-1.css')}}" rel="stylesheet" type="text/css">
+     <link href="{{ asset('dashboard/tpl/css_extend/lecture.css')}}" rel="stylesheet" type="text/css">
+
+
+     <link href="{{ asset('dashboard/tpl/css_extend/styles.css')}}" rel="stylesheet" type="text/css">
+     <link href="{{ asset('dashboard/tpl/css_extend/style.css')}}" rel="stylesheet" type="text/css">
+
+
+     <!-- external javascripts -->
+     <script src="{{ asset('dashboard/tpl/js/jquery-2.2.4.min.js')}}"></script>
+     <script src="{{ asset('dashboard/tpl/js/jquery-ui.min.js')}}"></script>
+     <script src="{{ asset('dashboard/tpl/js/bootstrap.min.js')}}"></script>
+     <!-- JS | jquery plugin collection for this theme -->
+     <script src="{{ asset('dashboard/tpl/js/jquery-plugin-collection.js')}}"></script>
+
+     <!-- Revolution Slider 5.x SCRIPTS -->
+     <script src="{{ asset('dashboard/tpl/js/revolution-slider/js/jquery.themepunch.tools.min.js')}}"></script>
+     <script src="{{ asset('dashboard/tpl/js/revolution-slider/js/jquery.themepunch.revolution.min.js')}}"></script>
+
+
+
+
+     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+     <!--[if lt IE 9]>
+     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+     <![endif]-->
+     <link href="{{ asset('dashboard/tpl/css/prince.css')}}" rel="stylesheet" type="text/css">
+     <link rel="stylesheet" href="{{ asset('dashboard/tpl/css/prince-2.css')}}">
+
+     <style type="text/css">
+
+         .dropdown-menu li {
+             position: relative;
+         }
+         .dropdown-menu .dropdown-submenu {
+             display: none;
+             position: absolute;
+             left: 100%;
+             top: -7px;
+         }
+         .dropdown-menu .dropdown-submenu-left {
+             right: 100%;
+             left: auto;
+         }
+         .dropdown-menu > li:hover > .dropdown-submenu {
+             display: block;
+         }
+     </style>
+
+     {{--        End Included Template Files--}}
+
+
+
+     @php
+    $version = App\Models\Language::version();
     @endphp
     @if($version->direction == 1)
     <!-- RTL css -->
@@ -56,11 +149,11 @@
  <body>
 
  	<!-- header -->
-    <header class="header-area header-three">  
+    <header class="header-area header-three">
        <div class="header-top second-header d-none d-md-block">
             <div class="container">
-                <div class="row align-items-center">      
-                   
+                <div class="row align-items-center">
+
                     <div class="col-lg-4 col-md-4 d-none d-lg-block ">
                         @if(isset($topbarSetting) && $topbarSetting->social_status == 1)
                         <div class="header-social">
@@ -83,8 +176,8 @@
                             @if(isset($socialSetting->youtube))
                             <a href="{{ $socialSetting->youtube }}" target="_blank"><i class="fab fa-youtube"></i></a>
                             @endif
-                           </span>                    
-                           <!--  /social media icon redux -->                               
+                           </span>
+                           <!--  /social media icon redux -->
                         </div>
                         @endif
                     </div>
@@ -117,12 +210,12 @@
                                </li>
                                @endisset
                             </ul>
-                        </div>                        
+                        </div>
                     </div>
-                    
+
                 </div>
             </div>
-        </div>    
+        </div>
 
 
         <div id="header-sticky" class="menu-area">
@@ -153,8 +246,8 @@
                         </div>
 
                         <div class="col-xl-3 col-lg-3 text-right d-none d-lg-block text-right text-xl-right">
-                            @php 
-                            $application = App\Models\ApplicationSetting::status(); 
+                            @php
+                            $application = App\Models\ApplicationSetting::status();
                             @endphp
                             @isset($application)
                             <div class="login">
@@ -168,7 +261,7 @@
                             </div>
                             @endisset
                         </div>
-                        
+
                         <div class="col-12">
                             <div class="mobile-menu"></div>
                         </div>
@@ -179,7 +272,7 @@
     </header>
     <!-- header-end -->
 
- 	
+
     <!-- Content Start -->
     @yield('content')
     <!-- Content End -->
@@ -190,13 +283,13 @@
         <div class="footer-top pb-70">
             <div class="container">
                 <div class="row justify-content-between">
-                    
+
                     <div class="col-xl-4 col-lg-4 col-sm-12">
                         <div class="footer-widget mb-30">
                             <div class="f-widget-title">
                                 <h2>{{ __('footer_socials') }}</h2>
                             </div>
-                            <div class="footer-social mt-10">                                    
+                            <div class="footer-social mt-10">
                                 @if(isset($socialSetting->facebook))
                                 <a href="{{ $socialSetting->facebook }}" target="_blank"><i class="fab fa-facebook-f"></i></a>
                                 @endif
@@ -215,7 +308,7 @@
                                 @if(isset($socialSetting->youtube))
                                 <a href="{{ $socialSetting->youtube }}" target="_blank"><i class="fab fa-youtube"></i></a>
                                 @endif
-                            </div>    
+                            </div>
                         </div>
                     </div>
 
@@ -233,8 +326,8 @@
                                     <li><a href="{{ route('login') }}" target="_blank">{{ __('field_staff') }} {{ __('field_login') }}</a></li>
                                     @endif
 
-                                    @php 
-                                    $application = App\Models\ApplicationSetting::status(); 
+                                    @php
+                                    $application = App\Models\ApplicationSetting::status();
                                     @endphp
                                     @isset($application)
                                     <li><a href="{{ route('application.index') }}" target="_blank">{{ __('navbar_admission') }}</a></li>
@@ -277,7 +370,7 @@
                             </div>
                         </div>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
@@ -299,8 +392,8 @@
                           </ul>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-4 col-12 text-center">          
-                        
+                    <div class="col-lg-4 col-md-4 col-12 text-center">
+
                     </div>
                     <div class="col-lg-4 col-md-4 col-12 text-center text-md-right">
                         @isset($setting->copyright_text)
