e44c7a44b815642b53680279f2582295e1d990d9
xmartina <martinsprince126@gmail.com>
2023-11-02 15:21:37 +0100

    fixed all files

--- resources/views/web/layouts/master.blade.php
+++ resources/views/web/layouts/master.blade.php
@@ -408,6 +408,27 @@
 
 
  	<!-- Script JS -->
+{{--        Included Libraries--}}
+    <!-- Footer Scripts -->
+    <!-- JS | Custom script for all pages -->
+    <script data-cfasync="false" src="{{ asset('dashboard/tpl/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js')}}"></script><script src="{{ asset('dashboard/tpl/js/custom.js')}}"></script>
+
+    <!-- SLIDER REVOLUTION 5.0 EXTENSIONS
+          (Load Extensions only on Local File Systems !
+           The following part can be removed on Server for On Demand Loading) -->
+    <script type="text/javascript" src="{{ asset('dashboard/tpl/js/revolution-slider/js/extensions/revolution.extension.actions.min.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('dashboard/tpl/js/revolution-slider/js/extensions/revolution.extension.carousel.min.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('dashboard/tpl/js/revolution-slider/js/extensions/revolution.extension.kenburn.min.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('dashboard/tpl/js/revolution-slider/js/extensions/revolution.extension.layeranimation.min.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('dashboard/tpl/js/revolution-slider/js/extensions/revolution.extension.migration.min.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('dashboard/tpl/js/revolution-slider/js/extensions/revolution.extension.navigation.min.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('dashboard/tpl/js/revolution-slider/js/extensions/revolution.extension.parallax.min.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('dashboard/tpl/js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('dashboard/tpl/js/revolution-slider/js/extensions/revolution.extension.video.min.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('dashboard/tpl/script/slick.slider.min.js')}}"></script>
+
+    <script type="text/javascript" src="{{ asset('dashboard/tpl/script/functions.js')}}"></script>
+{{--    End Included Libraries--}}
  	<script src="{{ asset('web/js/vendor/modernizr-3.5.0.min.js') }}"></script>
     <script src="{{ asset('web/js/vendor/jquery-3.6.0.min.js') }}"></script>
     <script src="{{ asset('web/js/popper.min.js') }}"></script>
