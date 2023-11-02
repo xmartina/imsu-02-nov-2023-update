0264a579e305e406f31fe0387b1f15f3e14c7865
xmartina <martinsprince126@gmail.com>
2023-11-02 17:33:54 +0100

    fixed

--- resources/views/web/layouts/master.blade.php
+++ resources/views/web/layouts/master.blade.php
@@ -93,7 +93,7 @@
                     </div>
 
                     <div class="col-lg-9 col-md-9 d-none d-lg-block text-right">
-                        <div class="header-cta d-flex justify-content-between align-items-center">
+                        <div class="header-cta d-flex align-items-center">
                             <ul>
                                <li class="mega cursor-pointer d-flex align-items-center justify-content-between"> Staff <span class="ml-2 material-symbols-outlined">expand_more</span>
                                    <ul class="d-none">
